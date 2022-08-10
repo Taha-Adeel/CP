d9562361df29516e5c25740ff77c0f81f65b12ad
Taha_Adeel <cs20btech11052@iith.ac.in>
2022-08-11 01:36:15 +0530

    Added my templates

--- My Templates/starter_template.cpp
+++ My Templates/starter_template.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 #define FAST ios::sync_with_stdio(0); cin.tie(0)
 template<class T> struct V: vector<T>{using vector<T>::vector;
-	void sort() {std::sort(this->begin(), this->end());}
+	void sort()     {std::sort(this->begin(), this->end());}
 	void sort_dsc() {std::sort(this->begin(), this->end(), greater<T>());}
 	long long sum() {long long sum = 0; for(auto& i: *this) sum += i; return sum;}
 	map<T, int> freqs() {map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
 	map<T, vector<int>> indices() {map<T, vector<int>> index; for(int i=0; i<(int)this->size(); i++) index[this->at(i)].push_back(i); return index;}
-	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v){out << i << ' ';} return out;}
-	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v){in >> i;} return in;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
 };
 #define pY {cout << "YES"; return;}
 #define pN {cout << "NO";  return;}
@@ -34,40 +34,17 @@ using vll = V<ll>;
 
 /*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
 
-struct Project{
-	int a;
-	int b;
-	int p;
-
-	friend bool operator<(const Project& p1, const Project& p2){return p1.b < p2.b;}
-};
-
 void solve(){
-	int n;
-	cin >> n;
-	V<Project> project(n);
-	FOR(i, n)
-		cin >> project[i].a >> project[i].b >> project[i].p;
-	project.pb({0, 0, 0});
-	project.sort();
-
-	map<int, ll> dp;
-	FOR1(i, n){
-		int a = (lower_bound(all(project), project[i].a, [](const Project& p, int a){return a > p.b;})-1)->b;
-		int b = project[i].b;
-
-		if(dp[b] == 0)
-			dp[b] = dp[project[i-1].b];
-
-		dp[b] = max(dp[b], dp[a] + project[i].p);
-	}
-
-	cout << dp[project[n].b];
+	
 }
 
 int main(){
-	solve();
-	cout << nl;
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
 	
 	return 0;
 }
