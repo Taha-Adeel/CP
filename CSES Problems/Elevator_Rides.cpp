5fa74f2de7f4ad6a235adcf90d2a3892d423159f
Taha_Adeel <cs20btech11052@iith.ac.in>
2022-08-12 02:00:39 +0530

    Added bitmap dp solution (elevator rides)

---
+++ CSES Problems/Elevator_Rides.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v){out << i << ' ';} return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v){in >> i;} return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	ll n, x;
+	cin >> n >> x;
+	vll w(n);
+	cin >> w;
+	w.sort();
+
+	V<pll> dp(1<<n); // dp[i] gives the value of {min no. of rides, min weight of the last ride} required for the subset given by the binary representation of i. 
+	dp[0] = {1, 0};
+
+	for(int s = 1; s < 1<<n; s++){
+		dp[s] = {n + 1, 0};
+		for(int i = 0; i < n; i++){
+			if(s & (1<<i)){
+				pll option = dp[s^(1<<i)];
+				if(option.S + w[i] <= x)
+					option.S += w[i];
+				else{
+					option.F++;
+					option.S = w[i];
+				}
+				dp[s] = min(dp[s], option);
+			}
+		}
+	}
+
+	cout << dp[(1<<n) - 1].F;
+}
+
+int main(){
+	solve();
+	cout << nl;
+	
+	return 0;
+}
