d67b7d10aaca24ff8fd67ae064847a82fcccb1db
Taha_Adeel <cs20btech11052@iith.ac.in>
2022-08-18 01:57:47 +0530

    Codeforces Round 814 (Div 2)

---
+++ Codeforces Round #814 (Div. 2)/C_Fighting_Tournament.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n, qn;
+	cin >> n >> qn;
+	vi a(n);
+	cin >> a;
+	V<pii> q(qn);
+	FOR(i, qn)
+		cin >> q[i].F >> q[i].S;
+
+	vi mx(n+1, 0);
+	FOR(i, n)
+		mx[i+1] = max(mx[i], a[i]);
+	
+	vi dist_to_next_bigger(n+1, 0);
+	FOR_REV(i, n-1){
+		if(a[i] < a[i+1])
+			dist_to_next_bigger[i] = 0;
+		else
+			dist_to_next_bigger[i] = dist_to_next_bigger[i+1] + 1;
+	}
+
+	FOR(ii, qn){
+		int i = q[ii].F - 1;
+		int k = q[ii].S;
+
+		int max_matches = (k - i) + (i != 0);
+		if(a[i] < mx[i]){
+			cout << 0 << nl;
+			continue;
+		}
+		if(a[i] == n){
+			cout << max_matches << nl;
+			continue;
+		}
+		cout << min(max_matches, dist_to_next_bigger[i]+(i != 0)) << nl;
+	}
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+	}
+	
+	return 0;
+}
