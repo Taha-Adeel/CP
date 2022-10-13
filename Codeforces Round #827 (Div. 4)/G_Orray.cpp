d72bd975a7d87a22a93679309dbe378a401e0a4c
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-10-13 22:29:40 +0530

    Codeforces Round #827 (Div. 4)

---
+++ Codeforces Round #827 (Div. 4)/G_Orray.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n;
+	cin >> n;
+	vi a(n);
+	cin >> a;
+
+	vi ans;
+	V<bool> used(n+1, false); used[n] = true;
+	int cur_or = 0;
+	for(ll mask = 1LL<<32; mask > 0; mask /= 2){
+		int max_or = 0, max_i = n;
+		FOR(i, n){
+			if(mask & a[i]){
+				if(!used[i] && ((cur_or | a[i]) > max_or)){
+					max_or = cur_or | a[i];
+					max_i = i;
+				}
+			}
+		}
+		if(!used[max_i]){
+			cur_or |= a[max_i];
+			used[max_i] = true;
+			ans.pb(a[max_i]);
+		}
+	}
+	FOR(i, n)
+		if(!used[i]) ans.pb(a[i]);
+	
+	cout << ans;
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
