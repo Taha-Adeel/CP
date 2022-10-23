c6e5718be78f5d4f8b28c8ef62d89d43651d5c3f
Taha_Adeel <cs20btech11052@iith.ac.in>
2022-10-24 05:00:50 +0530

    Codeforces Round #829 (Div. 2)

---
+++ Codeforces Round #829 (Div. 2)/D_Factorial_Divisibility.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{unordered_map<T, int> freq; freq.reserve(1e6); for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v){out << i << ' ';} return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v){in >> i;} return in;}
+};
+#define pY {cout << "Yes"; return;}
+#define pN {cout << "No";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	ll n, x;
+	cin >> n >> x;
+	vll a(n);
+	cin >> a;
+
+	auto freqs = a.freqs();
+	for(ll i = 1; i < x; i++){
+		freqs[i+1] += freqs[i]/(i+1);
+		if(freqs[i]%(i+1)) pN
+	}
+
+	pY
+}
+
+int main(){
+	solve();
+	cout << nl;
+	
+	return 0;
+}
