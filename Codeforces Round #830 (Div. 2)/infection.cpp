9adb107682b1fda55fb03d843627c9b185c3e7b0
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-10-24 06:22:22 +0530

    Practice problems

---
+++ Codeforces Round #830 (Div. 2)/infection.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n, m;
+	cin >> n >> m;
+	vector<int> infected(m);
+	for(int i = 0; i < m; i++)
+		cin >> infected[i];
+	
+	vector<int> closest_infected(n, n+1);
+	int infected_i = 0;
+	for(int i = 0; i < n; i++){
+		if(infected_i < m && infected[infected_i] == i+1) {
+			closest_infected[i] = 0;
+			infected_i++;
+		}
+		else if(i)
+			closest_infected[i] = closest_infected[i-1] + 1;
+	}
+	for(int i = n-2; i >= 0; i--)
+		closest_infected[i] = min(closest_infected[i], closest_infected[i+1] + 1);
+	
+	map<int, int> freqs;
+	for(int i = 0; i < n; i++)
+		freqs[closest_infected[i]]++;
+
+	// precompute factorials
+	vector<ll> fact(n+1, 1);
+	for(int i = 1; i <= n; i++)
+		fact[i] = (fact[i-1] * i) % MOD;
+	
+	ll ans = 1;
+	for(auto& freq: freqs){
+		if(freq.first == 0) continue;
+		ans = (ans * fact[freq.second])%MOD;
+	}
+
+	cout << ans;
+}
+
+int main(){
+	int T = 1;
+	// cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
