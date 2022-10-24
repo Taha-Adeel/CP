76361ff2a1316e67206fdd16f4719e1f6be1c898
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-10-24 06:22:09 +0530

    Codeforces Round #830 (Div. 2)

---
+++ Codeforces Round #830 (Div. 2)/C_2_Sheikh_Hard_Version.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	auto prefix_xors() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]^(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n, q;
+	cin >> n >> q;
+	vll a(n);
+	cin >> a;
+
+	auto prefix_sums = a.prefix_sums();
+	auto prefix_xors = a.prefix_xors();
+
+	auto f = [&](int l, int r){return (prefix_sums[r+1] - prefix_sums[l]) - (prefix_xors[r+1] ^ prefix_xors[l]);};
+
+	vi non_zero_indices;
+	FOR(i, n) if(a[i]) non_zero_indices.pb(i);
+
+	FOR(_, q){
+		int L, R;
+		cin >> L >> R; L--, R--;
+
+		int min_len = R-L+1, ans_l = L+1, ans_r = R+1;
+		vi is; 
+		for(auto it = lb(all(non_zero_indices), L); it != non_zero_indices.end() && *it <= R && is.size() <= 32; it++) 
+			is.pb(*it);
+		
+		for(auto& i: is){
+			int l = i, r = R;
+			while(l < r){
+				int m = (l+r)/2;
+				if(f(i, m) == f(L, R)) r = m;
+				else l = m + 1;
+			}
+			if(f(i, r) == f(L, R) && r-i+1 < min_len)
+				min_len = r-i+1,
+				ans_l = i+1,
+				ans_r = r+1;
+		}
+		if(is.empty()) ans_l = L+1, ans_r = L+1;
+
+		cout << ans_l << ' ' << ans_r << nl;
+	}
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+	}
+	
+	return 0;
+}
