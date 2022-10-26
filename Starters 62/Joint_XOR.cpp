1107fa7d7a0598a2c44ca5b6414cc845eea1ae56
Taha_Adeel <cs20btech11052@iith.ac.in>
2022-10-27 00:00:01 +0530

    Starters 62

---
+++ Starters 62/Joint_XOR.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+int mod_val(string s){
+	ll val = 0;
+	for(auto& c: s){
+		val <<= 1;
+		if(c == '1') val |= 1;
+		val %= MOD;
+	}
+	return val;
+}
+
+void solve(){
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+
+	int diff_i = -1, diff_count = 0;
+	FOR(i, n)
+		if(s[i] != s[0]){
+			diff_i = i;
+			while(i < n) {if(s[i] == s[diff_i]) diff_count++; else break; i++;}
+			break;
+		}
+	
+	if(diff_i == -1){ cout << 0; return;}
+	
+	int l1 = max(diff_i - diff_count, 0), l2 = diff_i;
+	string max_xor;
+	FOR(i, n - diff_i)
+		max_xor += '0' + (s[l1 + i] != s[l2 + i]);
+
+	cout << mod_val(max_xor);
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
---
+++ Starters 62/Pythagorean_Pair.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define FOR_RANGE(i, start, end) for(int i = start; i != end; i += (start<end) ? 1 : -1)
+#define FOR_REV(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+
+
+void solve(){
+	ll n;
+	cin >> n;
+
+	int two_pow = 0;
+	while(n%2 == 0) two_pow++, n /= 2;
+	if(two_pow%2) n *= 2, two_pow--;
+
+	for(ll i = 0; i * i < n; i++){
+		ll j = round(sqrt(n - i*i));
+		if(j*j == n - i*i){
+			cout << i * (1LL<<(two_pow/2)) << ' ' << j * (1LL<<(two_pow/2));
+			return;
+		}
+	}
+
+	cout << -1;
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
