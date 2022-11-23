ec6b4b2a11496c219507ebf94995ac5f9f65b281
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-11-24 01:29:52 +0530

    Starters 66

---
+++ STARTERS 66 DIVISION 1 (RATED TILL 5-STARS)/Good_Binary_Strings.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	string s;
+	cin >> s;
+
+	int _01s = 0, _10s = 0, n = s.size();
+	FOR(i, n-1)
+		if(s[i] == '0' && s[i+1] == '1') _01s++;
+		else if(s[i] == '1' && s[i+1] == '0') _10s++;
+
+	int ans = 0;
+	FOR(i, n){
+		int cur_01s = _01s, cur_10s = _10s;
+		if(s[i] == '0'){
+			if(i) 
+				if(s[i-1] == '1') cur_10s--;
+				else cur_01s++;
+			if(i < n-1)
+				if(s[i+1] == '1') cur_01s--;
+				else cur_10s++;
+		}
+		else{
+			if(i) 
+				if(s[i-1] == '0') cur_01s--;
+				else cur_10s++;
+			if(i < n-1)
+				if(s[i+1] == '0') cur_10s--;
+				else cur_01s++;
+		}
+		if(cur_01s == cur_10s) ans++;
+	}
+
+	cout << ans;
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
