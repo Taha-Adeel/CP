0e7c41b13874514ddb05bdba9dec91bce94f3385
Taha_Adeel <cs20btech11052@iith.ac.in>
2023-01-10 03:02:04 +0530

    Codeforces Round #842 (Div. 2) -> A,B,C

---
+++ Codeforces Round #842 (Div. 2)/C_Elemental_Decompress.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n;
+	cin >> n;
+	vi a(n), p(n, -1), q(n, -1);
+	cin >> a;
+
+	auto freq = a.freqs();
+	map<int, int> cur_freq;
+	map<int, int> pair;
+	FOR(i, n){
+		if(freq[a[i]] == 1) p[i] = q[i] = a[i];
+		else if(freq[a[i]] == 2){
+			if(cur_freq[a[i]]) p[i] = a[i];
+			else q[i] = a[i];
+			pair[a[i]] = -1;
+		}
+		else pN
+		cur_freq[a[i]]++;
+	}
+
+	set<int> unused;
+	FOR(i, n) 
+		if(freq[i+1] == 0)
+			unused.insert(-(i+1));
+	
+	FOR(i, n){
+		if(pair[a[i]] == -1){
+			auto it = unused.upper_bound(-a[i]);
+			if(it == unused.end()) pN;
+			pair[a[i]] = -(*it);
+			unused.erase(it);
+		}
+	}
+	FOR(i, n)
+		if(p[i] == -1) p[i] = pair[a[i]];
+		else if(q[i] == -1) q[i] = pair[a[i]];
+
+	cout << "YES" << nl;
+	cout << p << nl;
+	cout << q; 
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
