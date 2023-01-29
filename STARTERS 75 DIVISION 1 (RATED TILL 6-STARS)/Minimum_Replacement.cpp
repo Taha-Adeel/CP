0330fa6309a346ba73b96391b1063eabc458c2e3
Taha_Adeel <cs20btech11052@iith.ac.in>
2023-01-29 07:44:48 +0530

    STARTERS 75 DIVISION 1 (RATED TILL 6-STARS)

---
+++ STARTERS 75 DIVISION 1 (RATED TILL 6-STARS)/Minimum_Replacement.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n;
+	cin >> n;
+	vll a(n);
+	cin >> a;
+
+	ll sum = a.sum();
+	if(sum % 2) { cout << -1; return; }
+
+	ll ans = 0, x = sum/2;
+	ll l = 0, r = 1;
+	ll cur_sum = a[0];
+	while(l <= r){
+		if(cur_sum == x){
+			ans++;
+			if(r < n){
+				cur_sum += a[r];
+				r++;
+			}
+			else if(l < n){
+				cur_sum -= a[l];
+				l++;
+			}
+			else
+				break;
+		}
+		else if(cur_sum < x){
+			cur_sum += a[r];
+			r++;
+			if(r > n)
+				break;
+		}
+		else if(cur_sum > x){
+			cur_sum -= a[l];
+			l++;
+		}
+	}
+
+	if(ans > 0) pY
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
