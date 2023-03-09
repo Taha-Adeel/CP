6dd9e7643a0f1ddbf514bf0536f07091566c3843
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-03-09 22:55:52 +0530

    Codeforces Round 857 (Div. 2) -> A,B,C

---
+++ Codeforces Round 857 (Div. 2)/A_Likes.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n;
+	cin >> n;
+	vi b(n);
+	cin >> b;
+
+	FOR(i, n) b[i] = abs(b[i]);
+	auto freqs = b.freqs();
+
+	// Max number of likes
+	FOR(i, freqs.size()) cout << i+1 << ' ';
+	FOR(i, n - freqs.size()) cout << freqs.size()-i-1 << ' ';
+	cout << nl;
+
+	// Min number of likes
+	int freq_2_count = 0;
+	for(auto& [k, v]: freqs) if(v == 2) freq_2_count++;
+	
+	FOR(i, freq_2_count) cout << 1 << ' ' << 0 << ' ';
+	FOR(i, n - 2*freq_2_count) cout << i+1 << ' ';
+	cout << nl;
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+	}
+	
+	return 0;
+}
