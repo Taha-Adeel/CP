4cca6458074faec6a2103919789850c7e7735886
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-06-18 22:12:02 +0530

    Infero Placements Practice Contest 2 -> A, B, C, D, E, F, G, H, I

---
+++ Internships and Placements 2023-24 Practice Contest 2/I_Longest_k_Good_Segment.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v){out << i << ' ';} return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v){in >> i;} return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)            for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	int n, k; cin >> n >> k;
+	vi a(n); cin >> a;
+
+	// Find the longest segment with at most k distinct elements, using a frequency map and 2 pointers
+	map<int, int> freq;
+	int l = 0, r = 0, l_ans = 0, r_ans = 0, ans = 0;
+	while(r < n){
+		freq[a[r]]++;
+		while(freq.size() > k){
+			freq[a[l]]--;
+			if(freq[a[l]] == 0)
+				freq.erase(a[l]);
+			l++;
+		}
+		if(r-l+1 > ans){
+			ans = r-l+1;
+			l_ans = l;
+			r_ans = r;
+		}
+		r++;
+	}
+	cout << l_ans+1 << ' ' << r_ans+1 << nl;
+}
+
+int main(){
+	solve();
+	cout << nl;
+	
+	return 0;
+}
