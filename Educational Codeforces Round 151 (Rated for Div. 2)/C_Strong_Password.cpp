e87a9596f489e287ade203807247a9e3d1a10420
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-07-19 22:32:13 +0530

    Educational Codeforces Round 151 -> A,B,C

---
+++ Educational Codeforces Round 151 (Rated for Div. 2)/C_Strong_Password.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	string s; cin >> s;
+	int m; cin >> m;
+	string l, r; cin >> l >> r;
+
+	int n = s.size();
+	map<int, vi> index;
+	FOR(i, n) index[s[i]-'0'].pb(i);
+
+	vi cur_pwd_index(m, -1);
+	int i_pwd = 0;
+	while(i_pwd >= 0){
+		if(i_pwd == m) {i_pwd--; continue;}
+		
+		char i_char = (cur_pwd_index[i_pwd] == -1) ? l[i_pwd] : (s[cur_pwd_index[i_pwd]] + 1);
+		if(i_char > r[i_pwd]){ cur_pwd_index[i_pwd] = -1;  i_pwd--; continue;}
+		auto it = ub(all(index[i_char - '0']), (i_pwd > 0) ? cur_pwd_index[i_pwd-1] : -1);
+		if(it == index[i_char - '0'].end()) pY
+		else {cur_pwd_index[i_pwd] = *it; i_pwd++;}
+	}
+	pN
+}
+
+int main(){
+	FAST;
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
