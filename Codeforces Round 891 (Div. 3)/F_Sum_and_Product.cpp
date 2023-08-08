8e2228656c795452ca7b17250bc78fe613c88405
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-08-09 04:05:23 +0530

    Codeforces Round 891 (Div. 3) -> A,B,C,D,E,F

---
+++ Codeforces Round 891 (Div. 3)/F_Sum_and_Product.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, long long> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+bool is_perfect_square(ll D){
+	ll sq = sqrt(D);
+	return sq*sq == D || (sq+1)*(sq+1) == D || (sq-1)*(sq-1) == D;
+}
+
+void solve(){
+	int n; cin >> n;
+	vll a(n); cin >> a;
+
+	auto freqs = a.freqs();
+	int q; cin >> q;
+	while(q--){
+		ll x, y; cin >> x >> y;
+
+		ll D = x*x - 4*y;
+		if(D < 0 || !is_perfect_square(D)) {cout << 0 << ' '; continue;}
+
+		ll x1 = (x + sqrt(D))/2, x2 = (x - sqrt(D))/2;
+		if(D == 0)
+			cout << (freqs[x1] * (freqs[x1]-1))/2 << ' ';
+		else
+			cout << freqs[x1] * freqs[x2] << ' ';
+	}
+}
+
+int main(){
+	FAST;
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
