eaa9324c9b8ad8da158b25bdaab941591a5e4dce
Taha_Adeel <cs20btech11052@iith.ac.in>
2023-09-25 01:31:40 +0530

    Educational Codeforces Round 155 (Rated for Div. 2) -> A,B,C,D

---
+++ Educational Codeforces Round 155 (Rated for Div. 2)/C_Make_it_Alternating.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+	void sort()			{std::sort(this->begin(), this->end());}
+	void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+	auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+	auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+	auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+	auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+	friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+	friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 998244353;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+	string s; cin >> s;
+	ll n = s.size();
+
+	vll sim_seq_len;
+	ll cnt = 1;
+	FOR1(i, n-1){
+		if(s[i] == s[i-1]) cnt++;
+		else{
+			sim_seq_len.pb(cnt);
+			cnt = 1;
+		}
+	}
+	sim_seq_len.pb(cnt);
+
+	ll min_ops = n - sim_seq_len.size();
+	ll min_ops_perms = 1;
+	FOR1(i, min_ops) min_ops_perms = (min_ops_perms * i) % MOD;
+
+	for(auto& i: sim_seq_len){
+		min_ops_perms = (min_ops_perms * i) % MOD;
+	}
+
+	cout << min_ops << ' ' << min_ops_perms;
+}
+
+int main(){
+	int T;
+	cin >> T;
+	FOR(t, T){
+		solve();
+		cout << nl;
+	}
+	
+	return 0;
+}
