1411e729069be6b4354ddda21cac17395e723b09
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-10-12 00:18:30 +0530

    A,B,C,D

---
+++ Educational Codeforces Round 156 (Rated for Div. 2)/C_Decreasing_String.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()			{std::sort(this->begin(), this->end());}
+    void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+    auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+    auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+    auto indices()		{int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+    auto prefix_sums() 	{int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+    friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+    friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve(){
+    string s; cin >> s; 
+    ll n = s.size(), pos; cin >> pos;
+
+    ll x = 0, n_cur = n, chars = 0;
+    while(chars + n_cur < pos){
+        chars += n_cur;
+        n_cur--;
+        x++;
+    }
+
+    ll y = pos - chars - 1;
+    V<bool> present(n, true);
+    stack<pair<char, int>> st;
+    int i = 0;
+    while(x--){
+        if(st.empty()) st.push({s[i], i}), i++;
+        while(i < n && s[i] >= st.top().F) 
+            st.push({s[i], i}), i++;
+        if(st.empty()) break;
+        present[st.top().S] = false; st.pop();
+    }
+
+    FOR(i, n) if(present[i] && y-- == 0) 
+        cout << s[i];
+}
+
+int main(){
+    FAST;
+    int T;
+    cin >> T;
+    FOR(t, T){
+        solve();
+    }
+    
+    return 0;
+}
