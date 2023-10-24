703addda3ba0e433291699dd386b701c002ed6a5
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-10-25 02:53:36 +0530

    A,B,C,D,E,F,G

---
+++ ICPC 2023-24 Practice Contest 1/D_A_Leapfrog_in_the_Array.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()            {std::sort(this->begin(), this->end());}
+    void sort_dsc()     {std::sort(this->begin(), this->end(), greater<T>());}
+    auto sum()             {T sum = 0; for(auto& i: *this) sum += i; return sum;}
+    auto freqs()         {map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+    auto indices()        {int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii;}
+    auto prefix_sums()     {int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps;}
+    friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+    friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)               for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)                for(int i = (int)n-1; i >= 0; --i)
+#define all(v)                   v.begin(), v.end()
+#define rall(v)                  v.rbegin(), v.rend()
+
+#define F  first
+#define S  second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+
+ll solve(ll n, ll q) {
+    if (q % 2 == 0)
+        return q / 2 + 1;
+    else
+        return n/2 + solve((n+1)/2, q/2 + n%2);
+}
+ 
+int main() {
+    FAST;
+    ll n; cin >> n;
+ 
+    int q; cin >> q;
+ 
+    FOR(i, q){
+        ll p; cin >> p;
+        cout << solve(n, p - 1) << '\n';
+    }
+ 
+    return 0;
+}
