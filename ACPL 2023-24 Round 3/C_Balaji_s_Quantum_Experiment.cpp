e930c07d6c57a8968c8df97d14f1412724e1b7ff
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-11-10 00:54:00 +0530

    A,B,C,D,E

---
+++ ACPL 2023-24 Round 3/C_Balaji_s_Quantum_Experiment.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+int query(int l, int r) {
+    cout << "? " << l << ' ' << r << endl;
+    int ans; cin >> ans;
+    return ans;
+}
+
+void solve(){
+    int n; cin >> n;
+
+    vi ans(n);
+    int x12 = query(1, 2);
+    int x13 = query(1, 3);
+    int x23 = query(2, 3);
+
+    ans[1] = x12 + x23 - x13;
+    ans[0] = x12 - ans[1];
+    ans[2] = x23 - ans[1];
+
+    for(int i = 3; i < n; ++i) {
+        ans[i] = query(i, i+1) - ans[i-1];
+    }
+
+    cout << "! " << ans;
+}
+
+int main(){
+    FAST;
+    solve();
+    cout << endl;
+    
+    return 0;
+}
