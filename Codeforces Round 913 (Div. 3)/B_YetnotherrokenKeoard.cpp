0f5ccb5d4258402068ffe341e2fa7b295da5b95e
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2023-12-06 15:47:33 +0530

    ->A,B,C,D

---
+++ Codeforces Round 913 (Div. 3)/B_YetnotherrokenKeoard.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    string s; cin >> s;
+    stack<pair<int, char>> st_lower, st_upper;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == 'b') {
+            if(!st_lower.empty()) st_lower.pop();
+        }
+        else if(s[i] == 'B') {
+            if(!st_upper.empty()) st_upper.pop();
+        }
+        else {
+            if(islower(s[i])) st_lower.push({i, s[i]});
+            else st_upper.push({i, s[i]});
+        }
+    }
+    vector<pair<int, char>> ss;
+    while(!st_lower.empty()) {
+        ss.push_back(st_lower.top());
+        st_lower.pop();
+    }
+    while(!st_upper.empty()) {
+        ss.push_back(st_upper.top());
+        st_upper.pop();
+    }
+    sort(all(ss));
+
+    string ans = "";
+    for(auto& i: ss) 
+        ans += i.second;
+    
+    cout << ans;
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
