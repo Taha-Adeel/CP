4b204a507ca11bad405d8c877beea169d47d1f95
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-12-10 19:37:04 +0530

    Day 7 -> 1500, 1700

---
+++ Hard-75/E_2_Unforgivable_Curse_hard_version.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n, k; cin >> n >> k;
+    string s, t; cin >> s >> t;
+
+    if (2 * k > n) {
+        int l = n - k;
+        if(l < 0) {display(s == t)}
+        string s_mid = s.substr(l, n - 2*l);
+        string t_mid = t.substr(l, n - 2*l);
+        
+        if(s_mid != t_mid) pN;
+
+        s = s.substr(0, l) + s.substr(n - l, l);
+        t = t.substr(0, l) + t.substr(n - l, l);
+    }
+    sort(all(s));
+    sort(all(t));
+    
+    if(s == t) pY
+    else pN
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
---
+++ Hard-75/G_SlavicG_s_Favorite_Problem.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+struct Node {
+    V<pii> adj_list;
+};
+
+void solve() {
+    int n, a, b; cin >> n >> a >> b; --a, --b;
+    V<Node> tree(n);
+    FOR(i, n-1) {
+        int u, v, w; cin >> u >> v >> w; --u, --v;
+        tree[u].adj_list.pb({v, w});
+        tree[v].adj_list.pb({u, w});
+    }
+
+    set<int> path_a;
+    function<void(int, int, int)> dfs_a = [&](int u, int p, int x) -> void {
+        path_a.insert(x);
+        for(auto& [v, w]: tree[u].adj_list) {
+            if(v == p || v == b) continue;
+            dfs_a(v, u, x ^ w);
+        }
+    };
+    dfs_a(a, -1, 0);
+
+    bool found_ans = false;
+    function<void(int, int, int)> dfs_b = [&](int u, int p, int x) -> void {
+        if(found_ans) return;
+        if(u != b && path_a.count(x)) {
+            found_ans = true;
+            cout << "YES";
+            return;
+        }
+        for(auto& [v, w]: tree[u].adj_list) {
+            if(v == p) continue;
+            dfs_b(v, u, x ^ w);
+        }
+    };
+    dfs_b(b, -1, 0);
+
+    if(!found_ans) cout << "NO";
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
