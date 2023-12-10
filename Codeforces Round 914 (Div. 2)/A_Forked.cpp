5e867360ec29abf93947f9046954bfd3b7d43793
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-12-10 19:36:35 +0530

    -> Just A :skull:

---
+++ Codeforces Round 914 (Div. 2)/A_Forked.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int a, b; cin >> a >> b;
+    int xk, yk; cin >> xk >> yk;
+    int xq, yq; cin >> xq >> yq;
+
+    int dist_x = abs(xk - xq), dist_y = abs(yk - yq);
+    if(dist_x > dist_y) swap(dist_x, dist_y);
+    if(a > b) swap(a, b);
+
+    if(dist_x == 0) {
+        if(dist_y == 2*a || dist_y == 2*b) cout << 2;
+        else cout << 0;
+    }
+    else if(dist_x == 2*a && dist_y == 2*b) cout << 1;
+    else if(dist_x == dist_y) {
+        if(dist_x == a+b || dist_x == abs(b-a)) cout << 2;
+        else cout << 0;
+    }
+    else {
+        if(dist_x == abs(b - a) && dist_y == a+b) cout << 2;
+        else cout << 0;
+    }
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
