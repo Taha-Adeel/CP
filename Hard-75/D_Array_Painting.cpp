c70eea3ae3056349aa22e21125e539ce33d5b1c6
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-12-11 17:52:56 +0530

    Day 8 -> 1500, 1700

---
+++ Hard-75/D_Array_Painting.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); cin >> a;
+
+    int ans = 0;
+    int l = 0;
+    while(l < n) {
+        int r = l + 1;
+        bool has_2 = (a[l] == 2);
+        bool has_0 = false;
+        while(r < n) {
+            if(r - 1 > l && a[r - 1] == 0)
+                has_0 = true;
+            if(a[r] == 2)
+                has_2 = true;
+            bool good = (not has_0) and (has_2 or a[l] != 0 or a[r] != 0);
+            if(not good)
+                break;
+            r++;
+        }
+        l = r;
+        ans++;
+    }
+
+    cout << ans;
+}
+
+int main() {
+    FAST;
+    solve();
+    cout << nl;
+    
+    return 0;
+}
