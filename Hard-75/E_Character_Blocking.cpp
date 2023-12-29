aa5ec3d72c50623108cb7670afd800d61bb87c44
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2023-12-29 21:51:30 +0530

    Day 26 -> 1600, 1800

--- Hard-75/E_Character_Blocking.cpp
+++ Hard-75/E_Character_Blocking.cpp
@@ -34,27 +34,53 @@ using vll = V<ll>;
 /*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
 
 void solve() {
-    int n; cin >> n;
-    vi a(n), b(n); cin >> a >> b;
+    string s1, s2; cin >> s1 >> s2;
+    int t, q; cin >> t >> q;
 
-    auto check = [&](int ans) {
-        map<int, int> cnt;
-        for(int x: a) ++cnt[ x & ans];
-        for(int x: b) --cnt[~x & ans];
+    int blocked_cnt = 0, equal_cnt = 0, n = s1.size();
+    FOR(i, n) 
+        equal_cnt += (s1[i] == s2[i]);
 
-        bool ok = true;
-        for(auto [x, f]: cnt) if(f != 0)
-            ok = false;
-        
-        return ok;
-    };
-    
-    int ans = 0;
-    for(int bit = 29; bit >= 0; --bit) 
-        if(check(ans | (1 << bit)))
-            ans |= 1 << bit;
-    
-    cout << ans;
+    queue<pii> blocked_pos;
+    FOR(cur_time, q) {
+        // Check if any blocked position is unblocked
+        while(!blocked_pos.empty() && blocked_pos.front().first == cur_time) {
+            int pos = blocked_pos.front().second;
+            blocked_pos.pop();
+
+            if(s1[pos] == s2[pos]) equal_cnt++;
+            blocked_cnt--;
+        }
+
+        int type; cin >> type;
+        if(type == 1) { // Block the given position
+            int pos; cin >> pos; --pos;
+
+            if(s1[pos] == s2[pos]) equal_cnt--;
+            blocked_pos.push({cur_time + t, pos});
+            blocked_cnt++;  
+        }
+        else if(type == 2) { // Swap the given positions
+            int i1, pos1, i2, pos2; cin >> i1 >> pos1 >> i2 >> pos2; pos1--; pos2--;
+
+            if(s1[pos1] == s2[pos1]) equal_cnt--;
+            if(s1[pos2] == s2[pos2]) equal_cnt--;
+            if(i1 == i2) {
+                if(i1 == 1) swap(s1[pos1], s1[pos2]);
+                else swap(s2[pos1], s2[pos2]);
+            }
+            else {
+                if(i1 == 1) swap(s1[pos1], s2[pos2]);
+                else swap(s2[pos1], s1[pos2]);
+            }
+            if(s1[pos1] == s2[pos1]) equal_cnt++;
+            if(s1[pos2] == s2[pos2]) equal_cnt++;
+        }
+        else if(type == 3) { // Check if the unblocked positions are equal
+            if(n - blocked_cnt == equal_cnt) cout << "YES" << nl;
+            else cout << "NO" << nl;
+        }
+    }
 }
 
 int main() {
@@ -62,7 +88,6 @@ int main() {
     int T; cin >> T;
     FOR(t, T) {
         solve();
-        cout << nl;
     }
     
     return 0;
