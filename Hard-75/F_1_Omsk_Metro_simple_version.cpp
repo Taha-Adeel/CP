0e55d2088e772dedb83427b17d1d6ddb20d395b5
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2024-01-08 05:21:18 +0530

    Day 35 -> 1800, 2000

---
+++ Hard-75/F_1_Omsk_Metro_simple_version.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+
+    vi w, max_w_sum, min_w_sum, max_w, min_w;
+    w.pb(1); max_w_sum.pb(1); min_w_sum.pb(0); max_w.pb(1); min_w.pb(0);
+    FOR(i, n) {
+        char type; cin >> type;
+        if(type == '+') {
+            int vi, wi; cin >> vi >> wi; --vi;
+            w.pb(wi);
+            max_w_sum.pb(max(max_w_sum[vi] + wi, 0));
+            min_w_sum.pb(min(min_w_sum[vi] + wi, 0));
+            max_w.pb(max(max_w[vi], max_w_sum[vi] + wi));
+            min_w.pb(min(min_w[vi], min_w_sum[vi] + wi));
+        }
+        else {
+            int ui, vi, k; cin >> ui >> vi >> k; --ui, --vi;
+            if(k <= max_w[vi] && k >= min_w[vi]) cout << "YES" << nl;
+            else cout << "NO" << nl;
+        }
+    }
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
