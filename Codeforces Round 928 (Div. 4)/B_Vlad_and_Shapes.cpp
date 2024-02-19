47167c005105856ffae3ed1a936d2dceb4e53267
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2024-02-20 01:28:16 +0530

    -> A, B, C, D, E

---
+++ Codeforces Round 928 (Div. 4)/B_Vlad_and_Shapes.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    V<string> s(n); cin >> s;
+
+    int max_freq = 0, max_freq_idx = -1;
+    FOR(i, n) {
+        if (count(all(s[i]), '1') > max_freq) {
+            max_freq = count(all(s[i]), '1');
+            max_freq_idx = i;
+        }
+    }
+
+    int j = s[max_freq_idx].find('1');
+    bool square_up = false;
+    if (max_freq_idx > 0 && s[max_freq_idx - 1][j] == '1') square_up = true;
+    bool square_down = false;
+    if (max_freq_idx < n - 1 && s[max_freq_idx + 1][j] == '1') square_down = true;
+
+    if (square_up || square_down) cout << "SQUARE";
+    else cout << "TRIANGLE";
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
