cd81084c0d47743004354257d2bae0c64c260afd
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2024-03-14 00:08:06 +0530

    A, B, C

---
+++ Starters 125 Division 1 (Rated till 6-Stars)/Operating_on_A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    vll a(n), b(n); cin >> a >> b;
+
+    if (a.sum() != b.sum()) {
+        cout << "NO";
+        return;
+    }
+
+    vll ps_a = a.prefix_sums(), ps_b = b.prefix_sums();
+    vll ps_a_even, ps_a_odd, ps_b_even, ps_b_odd;
+    FOR(i, n) {
+        if (i % 2) {
+            ps_a_odd.pb(ps_a[i]);
+            ps_b_odd.pb(ps_b[i]);
+        } else {
+            ps_a_even.pb(ps_a[i]);
+            ps_b_even.pb(ps_b[i]);
+        }
+    }
+    ps_a_even.sort();
+    ps_a_odd.sort();
+    ps_b_even.sort();
+    ps_b_odd.sort();
+
+    if (ps_a_even == ps_b_even && ps_a_odd == ps_b_odd) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
