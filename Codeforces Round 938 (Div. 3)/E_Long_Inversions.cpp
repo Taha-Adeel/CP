d34bc6975bab39b5275817c072e13795dc45735d
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2024-04-09 00:14:38 +0530

    3 rounds

---
+++ Codeforces Round 938 (Div. 3)/E_Long_Inversions.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+
+    vi a(n);
+    FOR (i, n) {
+        a[i] = s[i] - '0';
+    }
+
+    auto possible = [&](int x) {
+        vi b = a;
+        vi ds(n + 1, 0);
+
+        int cur = 0;
+        FOR (i, n - x + 1) {
+            cur += ds[i];
+            if ((b[i] + cur) % 2 == 0) {
+                ds[i]++;
+                ds[i + x]--;
+                cur++;
+            }
+        }
+
+        bool ok = true;
+        FOR (i, x - 1) {
+            cur += ds[n - x + i + 1];
+            if ((b[n - x + i + 1] + cur) % 2 == 0) {
+                ok = false;
+                break;
+            }
+        }
+
+        return ok;
+    };
+
+    for (int x = n; x >= 0; x--) {
+        if (possible(x)) {
+            cout << x;
+            return;
+        }
+    }
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
