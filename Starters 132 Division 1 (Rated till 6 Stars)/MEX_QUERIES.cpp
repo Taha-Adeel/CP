d6405575d38a9f88afd9a494128d258bf2eed904
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2024-05-03 00:55:08 +0530

    A, B, C, D, E

---
+++ Starters 132 Division 1 (Rated till 6 Stars)/MEX_QUERIES.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    vi freq(n); cin >> freq;
+
+    vll dp(n + 1, -1); // dp[i] = min number of ops to make mex of array = i
+    dp[0] = freq[0];
+    freq.push_back(0);
+
+    for (int x = 1; x <= n; x++) {
+        if (freq[x - 1] == 0) {
+            int cur = x - 1, ops = 0;
+            while (cur > 0 && freq[cur] <= 1) {
+                cur /= 2;
+                ops++;
+            }
+            if (freq[cur] > 1) {
+                dp[x] = ops;
+                freq[cur]--;
+            } else {
+                break;
+            }
+        } else {
+            dp[x] = 0;
+        }
+        dp[x] += dp[x - 1] - freq[x - 1] + freq[x];
+    }
+
+    cout << dp;
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
