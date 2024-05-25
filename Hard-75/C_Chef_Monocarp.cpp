1c667d3ff9d46bf6063779ab0889faeb2036a421
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2024-05-26 03:58:39 +0530

    -> 1800, 2000

---
+++ Hard-75/C_Chef_Monocarp.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); cin >> a;
+
+    a.sort();
+    V<vi> dp(n, vi(2 * n, 1e9)); // dp[i][x] = min ans when considering first i elements & removing ith element at time x
+    FOR (x, 2 * n) {
+        dp[0][x] = abs(a[0] - (x + 1));
+    }
+    for (int i = 1; i < n; i++) {
+        FOR (x, 2 * n) {
+            for (int y = 0; y < x; y++) {
+                dp[i][x] = min(dp[i][x], dp[i - 1][y]);
+            }
+            dp[i][x] += abs(a[i] - (x + 1));
+        }
+    }
+
+    cout << *min_element(all(dp[n - 1]));
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
