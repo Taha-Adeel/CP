132e260e274643d65e77e912f4ee8db0cd9e6ddc
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2024-06-05 23:24:13 +0530

    -> A, B, C

---
+++ Starters 137 Division 1 (Rated till 5-Stars)/Double_Trouble.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    vi x(n), p(n); cin >> x >> p;
+
+    vi left(n, 0), right(n, 0);
+    left[0] = right[n-1] = 1;
+    for (int i = 1; i < n; i++) {
+        left[i] = left[i-1] && ((x[i] - x[i-1]) <= p[i]);
+    }
+    for (int i = n-2; i >= 0; i--) {
+        right[i] = right[i+1] && ((x[i+1] - x[i]) <= p[i]);
+    }
+
+    if (left[n-1] || right[0]) pY;
+    for (int i = 0; i < n - 1; i++) {
+        if (left[i] && right[i+1]) pY;
+    }
+
+    int cnt = 1, l = 0, r = n-1;
+    for (int i = 0; i < n-1; i++) {
+        if (cnt < 2) l = i;
+        if (x[i+1] > x[i] + p[i]) {
+            cnt++;
+        }
+    }
+    if (cnt <= 2) pY;
+
+    cnt = 1;
+    for (int i = n-1; i > 0; i--) {
+        if (cnt < 2) r = i;
+        if (x[i-1] < x[i] - p[i]) {
+            cnt++;
+        }
+    }
+    if (cnt <= 2) pY;
+    if (r - l <= 1) pY;
+
+    pN;
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+        cerr << nl;
+    }
+    
+    return 0;
+}
