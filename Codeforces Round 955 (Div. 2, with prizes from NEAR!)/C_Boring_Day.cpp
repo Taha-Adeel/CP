2bd2dbfc7e4ef62c9ae2639da4d9e94a5e8432cb
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2024-06-26 22:12:35 +0530

    -> A, B, C, D, E

---
+++ Codeforces Round 955 (Div. 2, with prizes from NEAR!)/C_Boring_Day.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    ll n, l, r; cin >> n >> l >> r;
+    vll a(n); cin >> a;
+
+    int li = 0, ri = 0, ans = 0;
+    while (ri < n) {
+        ll cur_sum = 0;
+        vll prefix_sums = {0};
+        while (ri < n && cur_sum + a[ri] < l) {
+            cur_sum += a[ri++];
+            prefix_sums.pb(cur_sum);
+        }
+
+        if (cur_sum >= l and cur_sum <= r) {
+            ans++;
+            li = ri;
+            continue;
+        }
+
+        while (ri <= n) {
+            ll x = *lower_bound(all(prefix_sums), cur_sum - r);
+            if (cur_sum - x >= l) {
+                ans++;
+                li = ri;
+                break;
+            }
+            if (ri < n) {
+                cur_sum += a[ri];
+                prefix_sums.pb(cur_sum);
+            }
+            ri++;
+        }
+    }
+
+    cout << ans;
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
