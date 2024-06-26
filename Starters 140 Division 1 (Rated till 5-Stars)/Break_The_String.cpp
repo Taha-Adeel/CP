9ee4aa7f0e2b795f84c467035606d0fc0aeaa1c9
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2024-06-26 22:12:45 +0530

    -> A, B, C, D, E

---
+++ Starters 140 Division 1 (Rated till 5-Stars)/Break_The_String.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+vi z_function(string s) {
+	int n = s.size();
+	vi z(n, 0);
+	for(int i = 1, l = 0, r = 0; i < n; ++i){
+		if(i <= r)
+			z[i] = min(r - i + 1, z[i - l]);
+		while(i + z[i] < n && s[z[i]] == s[i + z[i]])
+			++z[i];
+		if(i + z[i] - 1 > r)
+			l = i, r = i + z[i] - 1;
+	}
+	return z;
+}
+
+void solve() {
+    string s; cin >> s;
+
+    int n = s.size();
+    if (n % 2) {
+        cout << 0;
+        return;
+    }
+
+    vi z1 = z_function(s);
+    reverse(all(s));
+    vi z2 = z_function(s);
+
+    int ans = (z2[n / 2] >= n / 2);
+    for (int i = 0; i < n / 2; i++) {
+        if (z1[i + 1] < i + 1) continue;
+        if (z2[n / 2 - i - 1] >= n / 2 - i - 1) ans++;
+    }
+
+    cout << ans;
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
