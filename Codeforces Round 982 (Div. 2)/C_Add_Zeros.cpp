78c6e0e86c40189ed9977665e3bb8de3294264b4
Taha Adeel Mohammed <cs20btech11052@iith.ac.in>
2024-10-29 00:42:16 +0530

    -> A, B, C, D

---
+++ Codeforces Round 982 (Div. 2)/C_Add_Zeros.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n; cin >> n;
+    vll a(n); cin >> a;
+
+    for (int i = 0; i < n; i++) {
+        a[i] += i - n;
+    }
+
+    auto indices = a.indices();
+    vll dist(n, -1);
+    queue<int> q;
+    for (auto i: indices[0]) {
+        dist[i] = i;
+        q.push(i);
+    }
+    while(!q.empty()) {
+        int i = q.front(); q.pop();
+        for (auto j: indices[a[i] + i]) {
+            if (dist[j] == -1) {
+                dist[j] = dist[i] + j;
+                q.push(j);
+            }
+        }
+    }
+
+    cout << n + max(0LL, *max_element(all(dist)));
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
---
+++ Codeforces Round 982 (Div. 2)/D_1_The_Endspeaker_Easy_Version.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()        { std::sort(this->begin(), this->end()); }
+    void sort_dsc()    { std::sort(this->begin(), this->end(), greater<T>()); }
+    auto sum()         { T sum = 0; for(auto& i: *this) sum += i; return sum; }
+    auto freqs()       { map<T, int> freq; for(auto& i: *this) freq[i]++; return freq; }
+    auto indices()     { int n=this->size(); map<T, V<int>> ii; for(int i=0; i<n; i++) ii[(*this)[i]].push_back(i); return ii; }
+    auto prefix_sums() { int n=this->size(); V<T> ps(n+1,0); for(int i=0; i<n; i++) ps[i+1] = ps[i]+(*this)[i]; return ps; }
+    friend ostream& operator<<(ostream& out, const V<T>& v) { for(auto& i: v) out << i << ' '; return out; }
+    friend istream& operator>>(istream& in, V<T>& v)        { for(auto& i: v) in >> i; return in; }
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+#define display(x) if(x) pY else pN
+
+#define FOR(i, n)     for(int i = 0; i < (int)n; ++i)
+#define FOR1(i, n)    for(int i = 1; i <= (int)n; ++i)
+#define ROF(i, n)     for(int i = (int)n-1; i >= 0; --i)
+#define all(v)        v.begin(), v.end()
+#define pb push_back
+
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+/*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*/
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vll a(n), b(m); cin >> a >> b;
+
+    ll max_a = *max_element(all(a));
+    if (max_a > b[0]) {
+        cout << -1;
+        return;
+    }
+
+    vll ps = a.prefix_sums();
+
+    vll dp(n + 1, 1e18);
+    dp[0] = 0;
+    for (int j = 0; j < m; j++) {
+        for (int l = 0, r = 1; r <= n; r++) {
+            while ( l < r and ps[r] - ps [l] > b[j]) l++;
+            if (l < r) {
+                dp[r] = min(dp[r], dp[l] + m - j - 1);
+            }
+        }
+    }
+
+    cout << dp[n];
+}
+
+int main() {
+    FAST;
+    int T; cin >> T;
+    FOR(t, T) {
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
